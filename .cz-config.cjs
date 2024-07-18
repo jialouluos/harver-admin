4102b3e0dc7787982f8cc5d746fa4ac620ed4450
Haowen.li <180238813@qq.com>
2024-07-19 01:08:38 +0800

    perf(custom): fix(ddd)

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -15,26 +15,30 @@ module.exports = {
 		{ value: 'revert', name: 'revert:   回退' },
 		{ value: 'build', name: 'build:    打包' },
 	],
-	 // scope 类型（定义之后，可通过上下键选择）
-	 scopes: [
-        ['components', '组件相关'],
-        ['hooks', 'hook 相关'],
-        ['utils', 'utils 相关'],
-        ['styles', '样式相关'],
-        ['deps', '项目依赖'],
-        ['auth', '对 auth 修改'],
-        ['other', '其他修改'],
-        // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
-        ['custom', '以上都不是？我要自定义']
-    ].map(([value, description]) => {
-        return {
-            value,
-            name: `${value.padEnd(30)} (${description})`
-        }
-    }),
+	// scope 类型（定义之后，可通过上下键选择）
+	scopes: [
+		['components', '组件相关'],
+		['hooks', 'hook 相关'],
+		['utils', 'utils 相关'],
+		['styles', '样式相关'],
+		['deps', '项目依赖'],
+		['auth', '对 auth 修改'],
+		['other', '其他修改'],
+		// 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+		['custom', '以上都不是？我要自定义'],
+	].map(([value, description]) => {
+		return {
+			value,
+			name: `${value.padEnd(30)} (${description})`,
+		};
+	}),
+	scopeOverrides: {
+		fix: [{ name: 'feat' }, { name: 'style' }, { name: 'docs' }, { name: 'hooks' }],
+	},
 	// 消息步骤
 	messages: {
 		type: '请选择提交类型:',
+		scope: '选择一个 scope (可选)',
 		customScope: '请输入修改范围(可选):',
 		subject: '请简要描述提交(必填):',
 		body: '请输入详细描述(可选):',
@@ -42,6 +46,7 @@ module.exports = {
 		confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
 	},
 	// 跳过步骤
+	// allowCustomScopes: true,
 	skipQuestions: ['body', 'footer'],
 	// subject文字长度默认是72
 	subjectLimit: 72,
