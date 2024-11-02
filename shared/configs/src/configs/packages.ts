c6f105c2b61684db6aa6b2b43a446441fc6d8692
Haowen.li <180238813@qq.com>
2024-11-02 19:04:49 +0800

    尝试部署

--- shared/configs/src/configs/packages.ts
+++ shared/configs/src/configs/packages.ts
@@ -14,7 +14,7 @@ export default <Record<PACKAGE_ENUM, IPackagesConfig>>{
 		path: `/${PACKAGE_ENUM.DEMO}/`,
 		microConfig: {
 			name: `@jialouluo/${PACKAGE_ENUM.DEMO}`,
-			entry: `/${PACKAGE_ENUM.DEMO}/dist/index.html`,
+			entry: process.env.NODE_ENV === 'production' ? `/${PACKAGE_ENUM.DEMO}` : `//localhost:${PORT_ENUM.DEMO}`,
 			activeRule: `/${PACKAGE_ENUM.DEMO}`,
 			container: '#micro-container',
 			props: {},
@@ -51,7 +51,9 @@ export default <Record<PACKAGE_ENUM, IPackagesConfig>>{
 		path: `/${PACKAGE_ENUM.BLOG_ADMIN}/`,
 		microConfig: {
 			name: `@jialouluo/${PACKAGE_ENUM.BLOG_ADMIN}`,
-			entry: `/${PACKAGE_ENUM.BLOG_ADMIN}/dist/index.html`,
+			entry:
+				process.env.NODE_ENV === 'production' ? `/${PACKAGE_ENUM.BLOG_ADMIN}` : `//localhost:${PORT_ENUM.BLOG_ADMIN}`,
+
 			activeRule: `/${PACKAGE_ENUM.BLOG_ADMIN}`,
 			container: '#micro-container',
 			props: {},
